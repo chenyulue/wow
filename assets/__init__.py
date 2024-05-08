1d2c9249e7d50d8de07aa215510799becef22d29
chenyulue <chenyulue@163.com>
2024-05-08 23:21:21 +0800

    WOW 2018, Week 04

--- assets/__init__.py
+++ assets/__init__.py
@@ -6,6 +6,7 @@ CURDIR = Path(__file__).parent
 DATA_18W01 = CURDIR / "data" / "18w01_Looks_vs_Personality.csv"
 DATA_18W02 = CURDIR / "data" / "18W02_Sample_Superstore.csv"
 DATA_18W03 = DATA_18W02
+DATA_18W04 = DATA_18W02
 
 
 if __name__ == "__main__":
