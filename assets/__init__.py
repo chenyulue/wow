88008d3518bb2af68d2627d0029c7d3eda3e2b3b
chenyulue <chenyulue@163.com>
2024-05-23 23:13:06 +0800

    wow18, week 08

--- assets/__init__.py
+++ assets/__init__.py
@@ -10,6 +10,7 @@ DATA_18W04 = DATA_18W02
 DATA_18W05 = DATA_18W02
 DATA_18W06 = DATA_18W02
 DATA_18W07 = DATA_18W02
+DATA_18W08 = CURDIR / "data" / "18W08_babynames1950+.csv"
 
 
 if __name__ == "__main__":
